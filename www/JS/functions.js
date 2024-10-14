82bb6c8009a8554fbdb7f2f1d2a54d4c22d05982
Oleksandr Osaula <shurikosaula@gmail.com>
2024-10-14 22:06:53 +0300

    functions update

--- www/JS/functions.js
+++ www/JS/functions.js
@@ -16,3 +16,44 @@ function getSomeMessage(name){
     return `Hello dear ${name}`;
 }
 
+function logArguments(){
+    console.log(arguments);
+}
+logArguments('hello', 233, true);// зверни увагу - в самому меоді відсутні вхідні поля
+
+// при створенні змінної її можна ініціалізувати викликом методу:
+const logHello = function () {
+    console.log('Hello');
+}
+// така змінна викликається як метод
+logHello();
+
+// варіант №2
+const logHello2  = () => {
+  console.log('heelo from 2')
+}
+logHello2()
+
+const sum = (a, b) => a + b; // скорочення виразу. Те саме що - return a + b
+
+//метод можена викликати без відкривання дужок - ()
+console.log(sum) // консоль покаже вміст методу -  а саме: (a, b) => a + b
+
+// дозволяється переприсвоювання меотодів
+const fn1 = () => {
+    return 'hello from fn1';
+}
+
+const fn2 = fn1;
+console.log(fn2());// fn2 матиме поведінку так само як fn1
+
+// колбек методи працюють як методи з методами в параметрах котрі можна викликати відповідно в методі
+const fn3 = () => console.log('before');
+const fn4 = () => console.log('after');
+
+const printMessage = (actionBefore, actionAfter) => {
+    actionBefore();
+    console.log('hello');
+    actionAfter();
+}
+printMessage(fn3, fn4)
